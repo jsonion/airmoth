c7fb9ebb6957067a2145392f45c1475f1a879c78
js on ion <51760206+jsonion@users.noreply.github.com>
2023-07-13 09:48:32 +0200

    Create jstr.worldCurrencies.js

---
+++ jstr.worldCurrencies.js
@@ -0,0 +1,290 @@
+//  exported from https://en.wikipedia.org/wiki/Currency_symbol on 12th of July, 2023
+`
+؋, Af,Afs(1,+) | afghani
+Ar | ariary | Malagasy ariary
+฿, B | baht | Thai baht
+B/ | balboa | Panamanian balboa
+Br | birr | Ethiopian birr
+₿ | bitcoin | Bitcoin cryptocurrency
+Bs | bolívar | Venezuelan bolívar
+Bs | boliviano | Bolivian boliviano
+₵ | cedi | Ghanaian cedi
+₡, C | colon | Costa Rican colón
+C$ | córdoba | Nicaraguan córdoba
+D | dalasi | Gambian dalasi
+ден, DEN | denar | Macedonian denar
+дин, DIN | dinar | Serbian dinar
+.د.ج[rtl], DA | dinar | Algerian dinar
+.د.ب[rtl], BD | dinar | Bahraini dinar
+.د.ع[rtl], ID | dinar | Iraqi dinar
+.د.أ[rtl], JD | dinar | Jordanian dinar
+.د.ك[rtl], KD | dinar | Kuwaiti dinar
+.د.ل[rtl], LD | dinar | Libyan dinar
+.د.ت[rtl], DT | dinar | Tunisian dinar
+.د.م[rtl], DH, Dh,Dhs(1,+) | dirham | Moroccan dirham
+.د.إ[rtl], DH, Dh,Dhs(1,+) |dirham | Emirati dirham
+Db | dobra | São Tomé and Príncipe dobra
+
+$ | dollar
+    A$ | dollar | Australian dollar
+    B$ | dollar | Bahamian dollar
+    Bds$ | dollar | Barbadian dollar
+    BZ$ | dollar | Belize dollar
+    Ber$ | dollar | Bermudian dollar
+    B$ | dollar | Brunei dollar
+    Can$, C$ | dollar | Canadian dollar
+    CI$ | dollar | Cayman Islands dollar
+    EC$ | dollar | Eastern Caribbean dollar
+    FJ$ | dollar | Fijian dollar
+    G$ | dollar | Guyanese dollar[11]
+    HK$/元/圓 | dollar | Hong Kong dollar
+    J$ | dollar | Jamaican dollar
+    $ | dollar | Kiribati dollar
+    L$ | dollar | Liberian dollar
+    N$ | dollar | Namibian dollar
+    $NZ | dollar | New Zealand dollar
+    S$ | dollar | Singapore dollar
+    SI$ | dollar | Solomon Islands dollar
+    SRD | dollar | Surinamese dollar
+    NT$, 元, 圓 | dollar | New Taiwan dollar
+    TT$ | dollar | Trinidad and Tobago dollar
+    TV$ | dollar | Tuvaluan dollar
+    US$, $ | dollar | United States dollar
+
+    Arg$ | peso | Argentine peso
+    Ch$ | peso | Chilean peso
+    Col$ | peso | Colombian peso
+    Cu$ | peso | Cuban peso
+    RD$ | peso | Dominican peso
+    Mex$ | peso | Mexican peso
+    $U | peso | Uruguayan peso
+
+    $ | pataca | Macanese pataca
+
+₫, đ, Đ | dong | Vietnamese đồng
+֏ | dram | Armenian dram
+Esc | escudo | Cape Verdean escudo
+Ξ | ether | Ether cryptocurrency
+€ | euro | Euro
+
+ƒ, fl | florin
+    Afl, Aƒ | florin | Aruban florin
+    NAƒ, NAfl | florin | Netherlands Antillean guilder
+
+Ft | forint | Hungarian forint
+FBu | franc | Burundian franc
+
+F, Fr, fr | franc
+    fr.CFA | franc | Central African CFA franc
+    FC | franc | Comorian franc
+    FC | franc | Congolese franc
+    Fdj, DF | franc | Djiboutian franc
+    FG, GFr | franc | Guinean franc
+    FRw | franc | Rwanda franc
+    SFr | franc | Swiss franc
+    fr.CFA | franc | West African CFA franc
+
+G | gourde | Haitian gourde
+₲, Gs | guarani | Paraguayan guaraní
+₴, грн, hrn | hryvnia | Ukrainian hryvnia
+₭, K, KN | kip | Lao kip
+Kč | koruna | Czech crown
+
+kr | krone, krona
+    DKr | krone | Danish krone
+    NKr | krone | Norwegian krone
+    SKr | krona | Swedish krona
+    IKr | krona | Icelandic króna
+    kr | krona | Faroese króna
+
+kn, Kn | kuna | Croatian kuna
+Kz | kwanza | Angolan kwanza
+
+K | kina, kwacha
+    K | kina | Papua New Guinean kina
+    MK | kwacha | Malawian kwacha
+    ZK | kwacha | Zambian kwacha
+
+K,Ks(1,+) | kyat | Myanmar kyat
+₾ | lari | Georgian lari
+Lek, L | lek | Albanian lek
+L | lempira | Honduran lempira
+
+leu,lei(1,+) | leu
+    leu,lei(1,+), L(+) | leu |Romanian leu
+    leu,lei(1,+), L(+) | leu | Moldovan leu
+ 	
+Le | leone | Sierra Leonean leone
+лев,lev | lev | Bulgarian lev
+L,E(1,+) | lilangeni | Swazi lilangeni
+₺, TL | lira | Turkish liraN
+L,M(1,+) | loti | Lesotho loti
+₼, m, man | manat " Azerbaijani manat
+KM | mark | Bosnia and Herzegovina convertible mark
+Mt, Mtn | metical | Mozambican metical
+
+manat | manat | Turkmenistani manat
+Nfk, Nfa | nakfa | Eritrean nakfa
+₦, N | naira | Nigerian naira
+Nu | ngultrum | Bhutanese ngultrum
+UM | ouguiya | Mauritanian ouguiya
+T$ | paanga | Tongan paʻanga
+₱, P | peso | Philippine peso
+.ج.م[rtl], LE, £E | pound | Egyptian pound
+.ل.ل[rtl], LL, £L | pound | Lebanese pound
+
+LS | pound
+    .ج.س[rtl], £Sd | pound | Sudanese pound
+    .ل.س[rtl], £S, £Syr, SP | pound | Syrian pound
+
+£ | pound | Pound sterling
+SSP, £ | pound | South Sudanese pound
+P | pula | Botswana pula
+Q | quetzal | Guatemalan quetzal
+R | rand | South African rand
+R$| real | brazilian real
+﷼[rtl], IR, Rl,Rls(+) | rial | Iranian rial
+.ر.ي[rtl], YRl,YRls(1,+), Rl,Rls(1,+) | rial | Yemeni rial
+.ر.س[rtl], SR, SRl,SRls(1,+), Rl,Rls(1,+) | riyal | Saudi riyal
+.ر.ع[rtl], RO | rial | Omani rial
+.ر.ق[rtl], QR | rial | Qatari riyal
+៛, CR | riel | Cambodian riel
+RM | ringgit | Malaysian ringgit
+Rbl,Rbls(1,+), R(+) | rubel | Belarusian rubel
+₽, Rbl,Rbls(1,+) | ruble | Russian ruble
+ރ[rtl], Rf, MRf | rufiyaa | Maldivian rufiyaa
+₹, Re/Rs(1,+) | rupee | Indian rupee
+Re,Rs(1,+) | rupee
+    MRe, MRs | Mauritian rupee
+    रू, NRe, NRs | Nepalese rupee
+    PRe, PRs | Pakistani Rupee
+    SRe, SRs, SR | Seychellois rupee
+    රු, "SL Re","SL Rs"(1,+) | Sri Lankan rupee
+
+Rp | rupiah | Indonesian rupiah
+₪, NIS | shekel | Israeli new shekel
+TSh | shilling | Tanzanian shilling
+KSh | shilling | Kenyan shilling
+Sh.So. | shilling | Somali shilling
+USh | shilling | Ugandan shilling
+S/ | sol | Peruvian sol
+сом, som | som | Kyrgyzstani som
+৳, Tk | taka | Bangladeshi Taka
+WS$, T, ST | tala | Samoan tālā
+₸, T | tenge | Kazakhstani tenge
+₮, Tog | togrog | Mongolian tögrög
+VT | vatu | Vanuatu vatu
+
+₩ | won
+    ₩ | won | North Korean won
+    원, 圓, ₩ | won | South Korean won
+
+¥ | yuan, yen
+    元, 圆, ¥ | yuan | Renminbi yuan
+    円, 圓, ¥ | yen | Japanese yen
+
+zł, zl | zloty | Polish złot	Also zl when ł is unavailable
+
+! "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Double-barred_dollar_sign.svg/28px-Double-barred_dollar_sign.svg.png" (cifrão) | escudo | Portuguese escudo
+
+* rubla | ruble | Pridnestrovie rubla Transnistrial ruble is the currency of the internationally unrecognized state of Transnistria and is divided into 100 kopecks. not in Unicode
+
+* ¤ | generic | Generic placeholder for any actual symbol
+
+`, /////////////////////////////////////////////
+`
+¢   cent, centavo, etc.         Fraction
+A centesimal subdivision of the US dollar, the Canadian dollar and the Mexican peso     U+00A2 ¢ CENT SIGN
+c   cent etc. variant       Fraction
+In currencies Australian, New Zealand, South African dollar; the West African CFA centime, and divisions of the euro.
+See also: ¢
+
+Ch  chhertum    Bhutanese chhertum  Fraction
+A centesimal division of the ngultrum
+
+فلس[rtl] | fils | fils   Fraction
+1⁄1000 or 1⁄100 of various Arabic country currencies; see also falus
+
+gr | grosz | Polish grosz   Fraction
+A centesimal division of the złoty
+
+h | heller | Czech heller   Fraction
+A centesimal division of the koruna
+
+lp | lipa | Croatian lipa   Fraction
+A centesimal division of the kuna
+
+m, mil, Mil, mill, etc.     Fraction
+A millesimal subdivision of several currencies. As a subdivision of the US dollar the symbol ₥ is used (U+20A5 ₥ MILL SIGN)
+
+paisa   Indian paisa    Fraction
+Centesimal division of the Indian rupee. Before 2010, official sign was ps. Still used when Indian Paisa symbol.svg is not available.   Not in Unicode
+ps  paisa   Pakistani and Nepalese paisas   Fraction
+A centesimal division of the rupee
+p   penny   Penny sterling, and the pegged pennies of Alderney, the Falklands, Gibraltar, Guernsey, Jersey, Isle of Man and Saint Helena    Fraction
+The centesimal subdivision of a pound sterling, known as the "New Penny" when introduced in 1971
+piastre     Lebanese and Syrian piastres    A centesimal subdivision of the Lebanese and Syrian pounds
+
+PT | piastre | Egyptian and Sudanese piastres   Fraction
+A centesimal subdivision of the Egyptian and Sudanese pounds
+
+q | qintar | Albanian qintar    Fraction
+A centesimal subdivision of the lek
+
+tetri | tetri | Georgian lari   Fraction
+
+`, /////////////////////////////////////////////
+`
+₳ | austral | Argentine austral (1985–1991)
+Cz$ | cruzado | Brazilian cruzado (1986–1989)
+₢$ | cruzeiro | Brazilian cruzeiro (1942–1967)
+Cr$ | cruzeiro | Brazilian cruzeiro (1970–1986)
+Cr$ | cruzeiro | Brazilian cruzeiro (1990–1993)
+CR$ | cruzeiro | Brazilian cruzeiro real (1993–1994)
+NCz$ | cruzado | Brazilian cruzado novo (1989–1990)
+NCr$ | cruzeiro | Brazilian cruzeiro novo (1967–1970)
+Rs$ | real | Brazilian real (1747–1942)
+M | mark | East German Deutsche Mark (east) (1948–1964)
+DM | mark | West German and united German Deutsche Mark (west) (1948–2001)
+₻ | mark | Nordic mark symbol used by Ludvig Holberg in Denmark and Norway in the 17th and 18th centuries
+₯ | drachma | Greek drachma
+₠ | euro | ECU (not widely used, and now historical; replaced by the euro)
+Eº | escudo | Chilean escudo (1960–1975)
+ƒ | gulden | Dutch gulden, currently used in the Netherlands Antilles and Aruba
+Fr | franc | Franc, used in France and other countries; in France an F with double bar (₣) was proposed in 1988 but never adopted
+Kčs | koruna | Czechoslovak koruna (1919–1993)
+Lm | lira | Maltese lira
+Ls | lats | Latvian lats (1922–2013, not continuously)
+Lt | litas | Lithuanian litas (1922–2014, not continuously)
+M | mark | East German Mark der DDR (1968–1990)
+ℳ︁ | mark | German Mark (1875–1923)
+MDN | mark | East German Mark der Deutschen Notenbank (1964–1968)
+mk | mark | Finnish markka (1860–2002)
+o$s | peso | Argentine peso oro sellado (1881–1970)
+PF | peso | Philippine peso fuerte (1852–1901)
+₡ | colon | Salvadoran colón (1892–2001)
+₧ | peseta | Spanish peseta (1869–2002)
+R or RD | riksdaler | Swedish riksdaler (1777–1873)
+ℛ︁ℳ︁ | mark | Reichsmark (1923–1948)
+Sk | koruna | Slovak koruna (1993–2008)
+₷ | spesmilo | Spesmilo (1907 – First World War) in the Esperanto movement
+₶ | livre | Livre tournois (13th century – 1795)
+𐆚 | assarius | As coin used during the Roman Empire and Roman Republic
+𐆖 | denarius | Denarius coin used in Ancient Rome from 211 BC to the 3rd century AD
+𐆙 | dupondius | Dupondius coin used during the Roman Empire and Roman Republic
+𐆗 | quinarius | Quinarius coin used in Ancient Rome from 211 BC to the 3rd century AD
+𐆘 | sestertius | Sestertius coin used in Ancient Rome from 211 BC to the 3rd century AD
+I/. | inti Peruvian inti (1985-1991)
+৲ | rupee | Bengali rupee mark
+߾ | dorome | Dorome sign using the N'Ko alphabet
+߿ | taman | Taman sign using the N'Ko alphabet
+𞲰 | rupee | Indic Siyaq rupee mark
+
+`, /////////////////////////////////////////////
+`
+₰ | Pfennig, a subdivision of the German Mark (1875–1923) and the German Reichsmark (1923–1948)
+
+৹   Bengali ānā, historically used to represent 1/16 of a taka or rupee[30]
+
+৻   Bengali gaṇḍā, historically used to represent 1/20 of an ānā (1/320 of a taka or rupee)[30]
+`;
